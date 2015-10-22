b80d6e2d44c44df90e49c4463ad5c30929d7580b
Bryan Stenson <bryan.stenson@gmail.com>
2015-10-22 12:57:08 -0700

    doc update

--- doc.go
+++ doc.go
@@ -1,4 +1,10 @@
-/* This package provides interfaces, structs, and methods which model
-traditional playing cards (2 through A, all four suits).
+/*
+This package provides interfaces, structs, and methods which model traditional playing cards (2 through A, all four suits).
 */
 package playingcards
+
+import (
+  "github.com/stensonb/playingcards/card"
+  "github.com/stensonb/playingcards/deck"
+  "github.com/stensonb/playingcards/standarddeck"
+)
